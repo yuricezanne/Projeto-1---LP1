8b0c948e58a8414108f5b8c5411f3cdca58cb0a1
yuricezanne <yuricezanne@live.com>
2022-03-29 22:04:26 +0100

    Update Matriz

--- Projeto 1 - LP1/Program.cs
+++ Projeto 1 - LP1/Program.cs
@@ -1,10 +1,13 @@
-﻿public static void Main()
+﻿internal class Tabuleiro
 {
-    Random rnd = new Random();
-    List<string> myList = new List<string> { "normal", "snakes", "ladders", "cobra", "boost", "u-turn", "extra dice", "cheat dice" };
+    public static void Main()
+    {
+        Random rnd = new Random();
+        List<string> myList = new List<string> { "normal", "snakes", "ladders", "cobra", "boost", "u-turn", "extra dice", "cheat dice" };
 
-    var randomized = myList.OrderBy(item => rnd.Next());
+        var randomized = myList.OrderBy(item => rnd.Next());
 
-    Console.WriteLine($"Name normal: {string.Join(", ", myList)}");
-    Console.WriteLine($"Name suffled: {string.Join(", ", randomized)}");
+        Console.WriteLine($"Name normal: {string.Join(", ", myList)}");
+        Console.WriteLine($"Name suffled: {string.Join(", ", randomized)}");
+    }
 }
