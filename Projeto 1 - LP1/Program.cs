e30d393f820b8fed3d47b03f893667bbde431130
yuricezanne <yuricezanne@live.com>
2022-03-29 21:02:42 +0100

    Adicionar arquivos de projeto.

---
+++ Projeto 1 - LP1/Program.cs
@@ -0,0 +1,11 @@
+﻿List[Random.Range(1, List.Count)];
+class Tabuleiro
+{
+    int[] matrix = new int[5, 5] {"normal", "snakes", "ladders", "cobra", "boost", "u-turn", "extra dice", "cheat dice"};
+    Random rnd = new Random();
+    int index = rnd.Next(matrix.Length);
+    Console.WriteLine($"Name: {names[index]");
+}
+
+
+
